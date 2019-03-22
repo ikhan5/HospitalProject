ce2ca739a34e9e833d581768a3eff96b5093419f
Imzan Khan <ikhan5@lakeheadu.ca>
2019-03-22 11:10:27 -0400

    Add project files.

---
+++ HospitalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HospitalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
