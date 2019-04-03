20696c244c9ca061cd6c8736bfa7ed0bdcb1afc5
wngj0314@humbermail.ca <wngj0314@humbermail.ca>
2019-04-03 16:12:23 -0400

    added three features

---
+++ HospitalProject/Models/ReferAPatient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalProject.Models
+{
+    public class ReferAPatient
+    {
+        [Key]
+        public int ReferAPatientID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReferalDate { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Patient Name")]
+        public string PatName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        [Required, StringLength(255), Display(Name = "OHIP Number")]
+        public string OHIP { get; set; }
+
+        [Required, StringLength(int.MaxValue), Display(Name = "Patient Address")]
+        public string PatAddress { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Patient Phone")]
+        public string PatPhone { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Patient email")]
+        public string PatEmail { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Program Required")]
+        public string ProgReq { get; set; }
+
+        [Required, StringLength(int.MaxValue), Display(Name = "Current Primary Diagnosis")]
+        public string CurrPrimDiag { get; set; }
+
+        [Required, StringLength(int.MaxValue), Display(Name = "Medical History")]
+        public string MedHist { get; set; }
+
+        [Required, StringLength(int.MaxValue), Display(Name = "Service Required")]
+        public string ServiceReq { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Referring Facility")]
+        public string ReferFac { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Referring Physician Name")]
+        public string ReferPhysName { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Referring Physician Phone")]
+        public string ReferPhysPhone { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Referring Physician Email")]
+        public string ReferPhysEmail { get; set; }
+    }
+}
