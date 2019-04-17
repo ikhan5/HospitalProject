13d76ed50c11aaa073996c83c7badbb5d38bdada
wngj0314@humbermail.ca <wngj0314@humbermail.ca>
2019-04-17 14:03:36 -0400

    complete CRUD

--- HospitalProject/Controllers/ReferAPatientController.cs
+++ HospitalProject/Controllers/ReferAPatientController.cs
@@ -36,7 +36,7 @@ namespace HospitalProject.Controllers
             return View(await db.ReferAPatients.ToListAsync());
         }
 
-        // GET: ReferAPatient/Create
+        // Create referapatient
         public ActionResult Create()
         {
             return View();
@@ -45,7 +45,7 @@ namespace HospitalProject.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(
-        [Bind("Patient Name, WaitTime")]  ReferAPatient referapatient)
+        [Bind("MedHist,CurrPrimDiag,DOB,OHIP,PatAddress,PatEmail,PatName,PatPhone,ProgReq,ReferFac,ReferPhysEmail,ReferPhysName,ReferPhysPhone,ReferalDate,ServiceReq")]  ReferAPatient referapatient)
         {
             try
             {
@@ -64,5 +64,109 @@ namespace HospitalProject.Controllers
             }
             return View(referapatient);
         }
+
+        // edit referapatient
+        public ActionResult Edit(int? id)
+        {
+            if ((id == null) || (db.ReferAPatients.Find(id) == null))
+            {
+                return NotFound();
+            }
+            string query = "select * from ReferAPatients where ReferAPatientID=@id";
+            SqlParameter param = new SqlParameter("@id", id);
+            ReferAPatient myReferAPatient = db.ReferAPatients.FromSql(query, param).FirstOrDefault();
+            return View(myReferAPatient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, DateTime ReferalDate, string PatName, DateTime DOB, string OHIP, 
+            string PatAddress, string PatPhone, string PatEmail, string ProgReq, string CurrPrimDiag, 
+            string MedHist, string ServiceReq, string ReferFac, string ReferPhysName, string ReferPhysPhone, 
+            string ReferPhysEmail)
+        {
+            if ((id == null) || (db.ReferAPatients.Find(id) == null))
+            {
+                return NotFound();
+            }
+            string query = "update ReferAPatients set ReferalDate=@referaldate, PatName=@patname, DOB=@dob, OHIP=@ohip, PatAddress=@pataddress, PatPhone=@patphone, PatEmail=@patemail , ProgReq=@progreq, CurrPrimDiag=@currprimdiag, MedHist=@medhist, ServiceReq=@servicereq, ReferFac=@referfac, ReferPhysName=@referphysname, ReferPhysPhone=@referphysphone,ReferPhysEmail=@referphysemail" +
+                " where ReferAPatientID=@id";
+            SqlParameter[] myparams = new SqlParameter[16];
+            myparams[0] = new SqlParameter("@referaldate", ReferalDate);
+            myparams[1] = new SqlParameter("@patname", PatName);
+            myparams[2] = new SqlParameter("@dob", DOB);
+            myparams[3] = new SqlParameter("@ohip", OHIP);
+            myparams[4] = new SqlParameter("@pataddress", PatAddress);
+            myparams[5] = new SqlParameter("@patphone", PatPhone);
+            myparams[6] = new SqlParameter("@patemail", PatEmail);
+            myparams[7] = new SqlParameter("@progreq", ProgReq);
+            myparams[8] = new SqlParameter("@currprimdiag", CurrPrimDiag);
+            myparams[9] = new SqlParameter("@medhist", MedHist);
+            myparams[10] = new SqlParameter("@servicereq", ServiceReq);
+            myparams[11] = new SqlParameter("@referfac", ReferFac);
+            myparams[12] = new SqlParameter("@referphysname", ReferPhysName);
+            myparams[13] = new SqlParameter("@referphysphone", ReferPhysPhone);
+            myparams[14] = new SqlParameter("@referphysemail", ReferPhysEmail);
+            myparams[15] = new SqlParameter("@id", id);
+
+            db.Database.ExecuteSqlCommand(query, myparams);
+
+            return RedirectToAction("Index");
+        }
+
+        // details referapatient
+        public async Task<ActionResult> Details(int id)
+        {
+            if (db.ReferAPatients.Find(id) == null)
+            {
+                return NotFound();
+            }
+
+            ReferAPatient referapatient = await db.ReferAPatients.SingleOrDefaultAsync(d => d.ReferAPatientID == id);
+            return View(referapatient);
+        }
+
+
+        // delete referapatient
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ReferAPatient donform = db.ReferAPatients.Find(id);
+            if (donform == null)
+            {
+                return NotFound();
+            }
+
+            return View(donform);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ReferAPatient referapatient = await db.ReferAPatients.FindAsync(id);
+
+            if (referapatient.ReferAPatientID != id)
+            {
+                return Forbid();
+            }
+
+            db.ReferAPatients.Remove(referapatient);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
