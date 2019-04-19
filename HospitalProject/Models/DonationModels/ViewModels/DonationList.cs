76cd642c8ff8849119712b7813617311d493d0ba
wngj0314@humbermail.ca <wngj0314@humbermail.ca>
2019-04-19 13:43:56 -0400

    CRUD is working for volunteerpost and referapatient

--- HospitalProject/Models/DonationModels/ViewModels/DonationList.cs
+++ HospitalProject/Models/DonationModels/ViewModels/DonationList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HospitalProject.Models.DonationModels.ViewModels
+namespace HospitalProject.Models.VolunteerViewsModels.ViewModels
 {
     public class DonationList
     {
